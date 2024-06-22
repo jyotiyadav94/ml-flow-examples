debb63245e4992ed6f19239f40a27fb1678a4519
Jyoti Yadav <jyotiyadav79811@gmail.com>
2024-06-22 18:10:03 +0200

    updated

--- example.py
+++ example.py
@@ -4,14 +4,12 @@
 import logging
 import sys
 import warnings
-from urllib.parse import urlparse
-
 import numpy as np
 import pandas as pd
+from urllib.parse import urlparse
 from sklearn.linear_model import ElasticNet
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
-
 import mlflow
 import mlflow.sklearn
 from mlflow.models import infer_signature
